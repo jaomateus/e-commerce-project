030187943f2bac9b41a7e1bfaf18696d83293ff2
Joao Mateus <jaomateus@gmail.com>
2023-03-10 10:57:33 +0100

    implement about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+import { GiMachineGunMagazine, GiMartini } from "react-icons/gi";
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title='about'/>
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptatem debitis exercitationem quae, rem iusto non architecto expedita laudantium excepturi eos ratione aliquam consectetur doloribus minima assumenda eveniet. Enim, repellendus?</p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +48,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
